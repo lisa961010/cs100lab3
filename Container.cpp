3365ee18c21e49f2436e1214819c1cef257e8418
geyunru19961010@126.com <yge023@hammer.cs.ucr.edu>
2017-10-25 20:51:50 -0700

    added class Container

---
+++ Container.cpp
@@ -0,0 +1,103 @@
+#include "Container.h"
+
+
+
+
+
+
+
+void Container::set_sort_function(Sort* sort_function_get)
+
+{
+
+    sort_function = sort_function_get;
+
+}
+
+
+
+void Container::add_element(base* element)
+
+{
+
+
+
+}
+
+
+
+void Container::print()
+
+{
+
+
+
+}
+
+
+
+void Container::sort()
+
+{
+
+
+
+}
+
+
+
+void Container::swap(int i,int j)
+
+{
+
+
+
+}
+
+
+
+int Container::size()
+
+{
+
+
+
+}
+
+
+
+base* Container::at(int i)
+
+{
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Container.h
@@ -0,0 +1,69 @@
+fndef CONTAINER_H
+
+#define CONTAINER_H
+
+#include <cstddef>
+
+#include "Sort.h"
+
+#include "base.h"
+
+
+
+class Sort;
+
+class Container
+
+{
+
+    protected:
+
+    Sort* sort_function;
+
+
+
+    public:
+
+    /* Constructors */
+
+    Container() : sort_function(NULL){ }
+
+    Container(Sort* function) : sort_function(function){ }
+
+    /* Non Virtual Functions */
+
+    void set_sort_function(Sort* sort_function_get); // Set the type of sorting algorithm
+
+    /* Pure Virtual Functions */
+
+    // insert the top pointer of the tree into the container
+
+    virtual void add_element(base* element) = 0;
+
+    // iterate through trees and output values
+
+    virtual void print() = 0;
+
+    // calls on the previously set sorting-algorithm. Checks if sort_function is not null, throw exception if otherwise
+
+    virtual void sort() = 0;
+
+    /* Essentially the only functions needed to sort */
+
+    // switch tree locations
+
+    virtual void swap(int i,int j) = 0;
+
+    // get the top pointer of tree at index i
+
+    virtual base* at(int i) = 0;
+
+    // return the container size
+
+    virtual int size() = 0;
+
+};
+
+
+
+#endif // CONTAINER_H
